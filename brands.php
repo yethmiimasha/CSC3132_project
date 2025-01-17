a21c9cb768cfbfa07c7add5fed7986cc15c70a31
yethmiimasha <139648583+yethmiimasha@users.noreply.github.com>
2025-01-17 09:34:45 +0000

    update

---
+++ brands.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FOOTPRINT - Brands</title>
+    <link rel="stylesheet" href="styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <a href="#">
+                <img src="images/logo.jpg" alt="Footprint Logo">
+            </a>
+        </div>
+        <div class="logo">
+            <a href="#">FOOTPRINT</a>
+        </div>
+        <div class="search-container">
+            <input type="text" placeholder="Search for shoes">
+        </div>
+        <div class="auth-buttons">
+            <a href="login/login.php" class="btn-register">Login</a>
+            <a href="login/register.php" class="btn-register">Register</a>
+        </div>
+    </header>
+
+    <section class="brands">
+        <h2>Our Brands</h2>
+            <div class="card" style="width: 18rem;">
+                <img src="images/Bata.png" class="card-img-top" alt="Bata">
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="images/DSI.png" class="card-img-top" alt="DSI">
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="images/Usofto.png" class="card-img-top" alt="Usofto">
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="images/Waves.png" class="card-img-top" alt="Waves">
+            </div>
+        <h4>Discover top brands offering high-quality footwear, designed for style and comfort.</h4>
+    </section>
+
+    <footer>
+        <p>&copy; 2024 Footprint. All rights reserved.</p>
+        <div class="footer-links">
+            <a href="#">Privacy Policy</a> |
+            <a href="#">Terms of Service</a>
+        </div>
+    </footer>
+
+    <script src="script.js"></script>
+</body>
+</html>
