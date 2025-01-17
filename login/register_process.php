8b1091a6476681e62b931b0783a8eb927fbbd9b1
yethmiimasha <139648583+yethmiimasha@users.noreply.github.com>
2025-01-17 09:51:50 +0000

    updated

--- login/register_process.php
+++ login/register_process.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli('localhost', 'root', '', 'footprint');
+$conn = new mysqli('localhost', 'root', 'mariadb', 'footprint');
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
