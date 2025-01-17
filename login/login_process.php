9dc3c64a3f7502a9c3751d701182be8827aabc37
yethmiimasha <139648583+yethmiimasha@users.noreply.github.com>
2025-01-17 09:53:38 +0000

    uodated

--- login/login_process.php
+++ login/login_process.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$conn = new mysqli('localhost', 'root', '', 'footprint');
+$conn = new mysqli('localhost', 'root', 'mariadb', 'footprint');
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
